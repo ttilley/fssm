10573deca0eb27e08f59633fef410cacdf29eea2
Travis Tilley <ttilley@gmail.com>
2009-08-30 20:07:53 -0400

    switch the state object to using the cache tree

--- lib/fssm/state.rb
+++ lib/fssm/state.rb
@@ -1,13 +1,12 @@
 class FSSM::State
   def initialize(path, preload=true)
     @path = path
-    @snapshot = {}
-    snapshot if preload
+    @cache = FSSM::Cache.new
+    snapshot(@path.to_pathname) if preload
   end
   
-  def refresh
-    previous = @snapshot
-    current = snapshot
+  def refresh(base=nil)
+    previous, current = recache(base || @path.to_pathname)
     
     deleted(previous, current)
     created(previous, current)
@@ -30,16 +29,23 @@ class FSSM::State
     end
   end
   
-  def snapshot
-    snap = {}
-    @path.glob.each {|glob| add_glob(snap, glob)}
-    @snapshot = snap
+  def recache(base)
+    base = Pathname.for(base)
+    previous = @cache.files
+    snapshot(base)
+    current = @cache.files
+    [previous, current]
   end
   
-  def add_glob(snap, glob)
-    Pathname.glob(@path.to_pathname.join(glob)).each do |fn|
-      next unless fn.file?
-      snap["#{fn}"] = fn.mtime
+  def snapshot(base)
+    base = Pathname.for(base)
+    @cache.unset(base)
+    @path.glob.each {|glob| add_glob(base, glob)}
+  end
+  
+  def add_glob(base, glob)
+    Pathname.glob(base.join(glob)).each do |fn|
+      @cache.set(fn)
     end
   end
   
