dcd5c5556af2faf919c518be65fff6f35c11cc5d
Nathan Weizenbaum <nex342@gmail.com>
2010-02-06 14:46:41 -0800

    Use absolute paths for rb-inotify.
    This bumps the required rb-inotify version up to 0.5.1.
    
    Closes gh-4

--- lib/fssm/backends/inotify.rb
+++ lib/fssm/backends/inotify.rb
@@ -7,7 +7,7 @@ module FSSM::Backends
     def add_handler(handler, preload=true)
       @notifier.watch(handler.path.to_s, :attrib, :modify, :create,
         :delete, :delete_self, :moved_from, :moved_to, :move_self) do |event|
-        handler.refresh(FSSM::Pathname.for(event.name))
+        handler.refresh(FSSM::Pathname.for(event.absolute_name))
       end
 
       handler.refresh(nil, true) if preload
