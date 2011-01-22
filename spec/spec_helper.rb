cf7fda68d1ae3c837e3371c058d9658806b1a41a
Travis Tilley <ttilley@gmail.com>
2011-01-22 02:04:33 -0500

    Not quite sure what the rake task is doing, but it's firing off ruby with a load path that includes rspec, but not rubygems. It tweaks my ruby purity filter to ever have to do a require on rubygems, but the backends won't be found if rubygems isn't loaded.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'pathname'
+require 'rubygems'
 require 'fssm'
 
 require 'spec'
