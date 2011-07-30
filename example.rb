6056bac9dc7fb63651daff690c92ff2a08686498
Travis Tilley <ttilley@gmail.com>
2011-07-30 02:12:07 -0400

    describe a more realistic approach within the example.rb

--- example.rb
+++ example.rb
@@ -1,7 +1,4 @@
-$:.unshift(File.join(File.dirname(__FILE__), 'lib'))
-
-# for rb-inotify/rb-fsevent
-require 'rubygems'
+$:.unshift File.expand_path 'lib'
 
 require 'fssm'
 
@@ -10,3 +7,14 @@ FSSM.monitor('.', '**/*') do
   delete { |b, r| puts "Delete in #{b} to #{r}" }
   create { |b, r| puts "Create in #{b} to #{r}" }
 end
+
+# please note that for anything beyond simple block callbacks like above, you
+# will want to use the block parameter version of the API rather than the
+# automagical eval-in-context version:
+#
+# FSSM.monitor('.', '**/*') do |mon|
+#   mon.update { |b, r| puts "Update in #{b} to #{r}" }
+#   mon.delete { |b, r| puts "Delete in #{b} to #{r}" }
+#   mon.create { |b, r| puts "Create in #{b} to #{r}" }
+# end
+
